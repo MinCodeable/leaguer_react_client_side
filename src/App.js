c773384a083e7c920e9f968dfe3d5720a5cbeb00
mindy <minmaglh@gmail.com>
2022-04-23 18:55:21 -0500

    styled-components added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route index element={<Home/>}/>
+          <Route index element={<Champions/>}/>
           <Route path="champions" > 
             <Route index element={<Champions/>}/>
             <Route path=":id" element={<ChampionDetail/>}/>
