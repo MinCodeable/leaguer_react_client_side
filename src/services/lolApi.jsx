749554edc1647ee8fd7590800d4a994816a2877a
mindy <minmaglh@gmail.com>
2022-05-01 23:24:55 -0500

    fix base url

--- src/services/lolApi.jsx
+++ src/services/lolApi.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const lolApi = createApi({
     reducerPath:'lolApi',
-    baseQuery:  fetchBaseQuery({ baseUrl: 'https://lol-server-side.herokuapp.com//api/v1/'}),
+    baseQuery:  fetchBaseQuery({ baseUrl: 'https://lol-server-side.herokuapp.com/api/v1/'}),
     endpoints:  (build) => ({
                     getChampions: build.query({
                         query:  ({page=1, textSearch="", type_champ="", difficulty="", order="asc", limit=5}) => `champions?pag=${page}&txt=${textSearch}&type=${type_champ}&dif=${difficulty}&cond=${order}&limit=${limit}`
